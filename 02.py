5de4637f8cb81f8164fd5c0134c0c517a6ad0d09
Brice Waegeneire <brice.wge@gmail.com>
2017-12-03 16:38:07 +0100

    day 02 working

--- 02.py
+++ 02.py
@@ -5,10 +5,10 @@ import os
 
 with open(sys.argv[1]) as f:
     puzzle = f.readlines()
-print(puzzle)
 
 total = 0
 for line in puzzle:
-    print(line)
-    total += int(max(line.split())) - int(min(line.split()))
+    row = [int(x) for x in line.split()]
+    total += max(row) - min(row)
+
 print(total)
